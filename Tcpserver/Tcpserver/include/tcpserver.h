5d9768d677d94b6fa102db167ab8f4e1519901af
pzy123456 <737230416@qq.com>
2020-01-24 07:38:53 +0800

    Tcp模式下打开客户端监听服务器互发数据

---
+++ Tcpserver/Tcpserver/include/tcpserver.h
@@ -0,0 +1,22 @@
+#ifndef __TCPSERVER_H__
+#define __TCPSERVER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ATaskTcpServer( void *pvParameters );
+
+void TcpServer_init(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
+
+
+
